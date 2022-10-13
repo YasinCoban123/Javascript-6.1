ca0c129543791789713eaa3a5a6feebb026082c2
YasinCoban123 <9018681@student.zadkine.nl>
2022-10-13 12:58:04 +0200

    YC

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+alert("Je madre");
+// kleur aanpassen
+//document.getElementById("title").style.color = "red";
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 + getal2;
+alert("het antwoord is: " + som);
+
+// promptbox
+var kleur = prompt("geef een engelse kleurnaam: (red or blue", "");
+
+document.getElementById("title").style.color = kleur ;
